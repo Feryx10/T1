55cacd78df46731a16cdb1c4789a1ba0b90ddabe
Feryx10 <fapm10@gmail.com>
2018-10-26 19:34:11 -0300

    Update T1.java

--- T1.java
+++ T1.java
@@ -14,11 +14,12 @@ import java.util.*;
 public class T1 {
 
     public static void main(String[] args) {
-       Scanner reader = new Scanner (System.in);        
+       Scanner reader = new Scanner (System.in);
        String regular = reader.next();
-        
+       String chain = reader.next();
         
         
     }
     
 }
+
