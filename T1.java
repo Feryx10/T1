ed478cd9a331c9c9080a7341403c0a14ffb5b3a5
Feryx10 <fapm10@gmail.com>
2018-10-26 19:19:29 -0300

    Update T1.java

--- T1.java
+++ T1.java
@@ -1,1 +1,24 @@
-# T1
+package t1;
+
+/**
+ *
+ * @author 
+ * Luciano Garcia 
+ * Fernando Pino 
+ *
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class T1 {
+
+    public static void main(String[] args) {
+       Scanner reader = new Scanner (System.in);
+       String regla = 
+        
+        
+        
+    }
+    
+}
