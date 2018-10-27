678dada73f23f024211298e099b269cb60af8d98
Feryx10 <fapm10@gmail.com>
2018-10-27 00:54:42 -0300

    Update Converter.java

--- Converter.java
+++ Converter.java
@@ -11,4 +11,12 @@ public abstract class Converter implements Convert {
     public Converter(){
     }
     
+    @Override
+    public void toAFND(String[] regular,String[] chain){        
+    }
+    
+    @Override
+    public void toAFD (String[] regular,String[] chain){        
+    }
+    
 }
