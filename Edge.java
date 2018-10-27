e7878073c4e5ef25a2cb83b7150b246771d0b43c
Feryx10 <fapm10@gmail.com>
2018-10-27 01:15:36 -0300

    Create Edge.java

---
+++ Edge.java
@@ -0,0 +1,22 @@
+/**
+ * Maquinas Abstractas. 2018-2
+ * Tarea 1 
+ * @author 
+ * Luciano Garcia 
+ * Fernando Pino 
+ */
+
+public class Edge {   
+
+    private final String father;
+    private final String value;
+    private final String son;
+    
+    public Edge(String father,String value,String son){
+        this.father = father;
+        this.value = value;
+        this.son = son;
+        
+    }
+    
+}
