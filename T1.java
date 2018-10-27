422e74695a443796f808a46d1fb50850e673f282
Feryx10 <fapm10@gmail.com>
2018-10-26 23:42:25 -0300

    Update T1.java

--- T1.java
+++ T1.java
@@ -11,20 +11,17 @@ import java.util.*;
 
 public class T1 {
 
-    public static void main(String[] args) throws FileNotFoundException{
-        
+    public static void main(String[] args) throws FileNotFoundException { 
+               
+       // File file = new File(args[0]);
        Scanner reader = new Scanner (args[0]);       
        Converter temp = new Converter();
        
        String regular = reader.next();
-       String chain   = reader.next();
+       String chain   = reader.next();    
        
-       String[] aux1  = regular.split("");
-       String[] aux2  = regular.split("");
-       int i = 0;
-       int j = 0;
-       while(aux1.length==i && aux1.length==j)
-       System.out.print(aux1[i] + "  " + aux2[j]);   
+       System.out.print(regular+ "  " + chain); 
+       
         
     }
     
