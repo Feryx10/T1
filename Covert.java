20d709bfef28e3f1330d6c86a40fcbaa52294547
Feryx10 <fapm10@gmail.com>
2018-10-27 00:31:41 -0300

    Create Covert.java

---
+++ Covert.java
@@ -0,0 +1,16 @@
+/**
+ * Maquinas Abstractas. 2018-2
+ * Tarea 1 
+ * @author 
+ * Luciano Garcia 
+ * Fernando Pino 
+ */
+
+public interface Convert {
+    
+    public void toAFND(String[] array);
+    
+    public void toAFD (String[] array);
+    
+    
+}
