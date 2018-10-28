564840d29644c279fbf9101f461bbf80e4958031
lucianoags <lnicolas15@alumnos.utalca.cl>
2018-10-28 19:57:43 -0300

    Update Funciones.java

--- Funciones.java
+++ Funciones.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package maquinasabstractas;
+package tarea1;
 
 import java.util.ArrayList;
 
@@ -15,18 +15,42 @@ public class Funciones {
     ArrayList<Edge> edges = new ArrayList<Edge>();
     ArrayList<Edge> dones = new ArrayList<Edge>();
     private String[] expression;
+    int finReal;
+    int inicioReal;
     
     
     
     public void desarmador(int inicio, int fin)
     {
-        while(fin<inicio)
+        finReal=fin;
+        inicioReal=inicio;
+        while(fin>inicio)
         {
             contieneSimbolo(fin);
+           // System.out.println(expression[fin]);
             fin--;
         }
     }
     
+    public void parse()
+    {
+        
+       // edges.add(new Edge(ultimoNodo(),"a",ultimoNodo()+1));
+       // edges.add(new Edge(ultimoNodo()-1,"b",ultimoNodo()));
+       // dones.add(new Edge(ultimoNodo()-3, "done", ultimoNodo()-1));
+        
+        String s="(a.b.c.d.e)(a.b)";
+        expression=s.split("");
+        for(int i=0;i<expression.length;i++)
+        {
+            System.out.print(expression[i]);
+        }
+        System.out.println("");
+        System.out.println("--------");
+       // expression[1]="done0";
+        desarmador(0, 10);
+        imprimir();
+    }
     
     private void contieneSimbolo(int fin) 
     {
@@ -43,7 +67,16 @@ public class Funciones {
 
     private void concatenacion(int fin) 
     {
-        if(expression[fin+1].length()>1)
+        int tempFin=finReal;
+        
+        if (expression[fin-1].length()>1 && expression[fin+1].length()>1)
+        {
+            int doneUno = getDone(expression[fin-1]);
+            int doneDos = getDone(expression[fin+1]);
+            edges.add(new Edge(dones.get(doneUno).getSon(), "_", dones.get(doneDos).getFather()));
+            dones.add(new Edge(dones.get(doneUno).getFather(), "done", dones.get(doneDos).getSon()));
+        }
+        else if(expression[fin+1].length()>1)
         {
             int done = getDone(expression[fin+1]);
             edges.add(new Edge(ultimoNodo(), expression[fin-1], dones.get(done).getFather()));
@@ -55,10 +88,24 @@ public class Funciones {
             edges.add(new Edge(dones.get(done).getSon(), expression[fin+1], ultimoNodo()));
             dones.add(new Edge(dones.get(done).getFather(), "done",ultimoNodo()-1 ));
         }
+        
         else
         {
-            edges.add(new Edge(ultimoNodo(), ))
+            edges.add(new Edge(ultimoNodo(),expression[fin-1],ultimoNodo()+1));
+            edges.add(new Edge(ultimoNodo()-1,expression[fin+1],ultimoNodo()));
+            dones.add(new Edge(ultimoNodo()-3, "done", ultimoNodo()-1));
         }
+        expression[fin]=("done"+String.valueOf(dones.size()-1));
+        boolean punto = false;
+        while(!punto)
+        {
+            expression[tempFin]=("done"+String.valueOf(dones.size()-1));
+            tempFin--;
+            if(tempFin<inicioReal || expression[tempFin].equals("."))
+                punto=true;
+        }
+       
+        
     }
 
     private void estrella(int fin) {
@@ -82,7 +129,22 @@ public class Funciones {
         return mayor+1;
     }
 
-    private int getDone(String string) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private int getDone(String string) 
+    {
+        String temp[] = string.split("");
+        return Integer.parseInt(temp[temp.length-1]);
+    }
+    public void imprimir()
+    {
+        for(int i=0; edges.size()>i; i++)
+        {
+            System.out.println(edges.get(i).getFather()+","+edges.get(i).getValue()+","+edges.get(i).getSon());
+        }
+        System.out.println("------------");
+        for(int i=0; i<expression.length;i++)
+        {
+            
+            System.out.print(expression[i]);
+        }
     }
 }
