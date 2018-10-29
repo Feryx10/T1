e242e8ce9c7b54c33dca5dba51f9c8204b181139
lucianoags <lnicolas15@alumnos.utalca.cl>
2018-10-29 20:27:28 -0300

    Update Converter.java

--- Converter.java
+++ Converter.java
@@ -7,18 +7,20 @@
  */
 
 import java.util.*;
-import java.util.regex.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Converter implements APIConvert , APISearch{
     
-    
+    ArrayList<String> sigma = new ArrayList<>();  
     @Override
     public void toAFND(String[] regular,String[] chain){
         
-        Funciones funciones = new Funciones(regular);
+        String[] aux;
+        aux = regular.clone();
+        Funciones funciones = new Funciones(regular, toSigma(aux));
         int i = 0;                
-        ArrayList<String> sigma = new ArrayList<>();     
-        ArrayList<Integer> group = new ArrayList<>();         
+        ArrayList<Integer> group = new ArrayList<>(); 
         while(i!=positions(regular,"(").length)
         {
             int open  = positions(regular,"(")[i]; 
@@ -32,18 +34,33 @@ public class Converter implements APIConvert , APISearch{
                      close = positions(regular,")")[j--];
             group.add(open);  
             group.add(close);
-            System.out.println(open+" "+close);
-            System.out.println(group.toString());            
             i++;            
-        }        
-        System.out.println("");
+        }
+        
+        
         
         for(int k=0; k < group.size() ;k=k+2)
-        {           
+        {
             funciones.desarmador(group.get(k), group.get(k+1));
         }
-             
-               
+        int total=funciones.totalNodos();
+        
+        System.out.print("K={");
+        for (int j=0; j <= total; j++)
+        {
+            if (j==total)
+                System.out.print("q"+j+"}");
+            else
+                System.out.print("q"+j+",");
+        }
+        System.out.println("");
+        toSigma(aux);
+        printSigma();
+        funciones.completeStart();
+        printEdges(funciones);
+        printStartFinish(funciones.getFinishEnd());
+        
+        
     }
     
     
@@ -66,21 +83,57 @@ public class Converter implements APIConvert , APISearch{
                temp[count]=i;
                count++;
             }  
-        return temp;        
-    } 
+        return temp;
+    }
+    
+    public void getSigma(String[] regular)
+    {
+        for(int i=0; regular.length >i ;i++)
+        {
+            
+        }
+    }
+    
+    public void printEdges(Funciones funciones)
+    {
+        System.out.println("");
+        System.out.println("Delta:");
+        ArrayList<Edge> edges = funciones.getEdges();
+        for(int i=0; i<edges.size(); i++)
+        {
+            System.out.println("(q"+edges.get(i).getFather()+","+edges.get(i).getValue()+",q"+edges.get(i).getSon()+")");
+        }
+    }
+    
+    public void printStartFinish(Edge edge)
+    {
+        System.out.println("s=q"+edge.getFather());
+        System.out.println("F={q"+edge.getSon()+"}");
+    }
     
     @Override
-    public ArrayList<String> toSigma(String[] array){  
-        ArrayList<String> temp = new ArrayList<>();        
+    public ArrayList<String> toSigma(String[] array){    
         String aux = Arrays.toString(array);
         Pattern pattern = Pattern.compile("[a-zA-Z0-9]", Pattern.MULTILINE);
         Matcher matcher = pattern.matcher(aux);
         while (matcher.find()) {
-            if(!temp.contains(matcher.group())) 
-                temp.add(matcher.group());   
+            if(!sigma.contains(matcher.group())) 
+                sigma.add(matcher.group());   
         }
-        return temp;
-    } 
-    
-    
+        
+       
+      //  System.out.println("");
+      //  System.out.print("Sigma =" + temp.toString() + " ");
+      //  System.out.println("");
+        return sigma;
+    }
+    public void printSigma()
+    {
+        System.out.print("Sigma={");
+        for(int i=0; i<sigma.size()-1;i++)
+        {
+            System.out.print(sigma.get(i)+",");
+        }
+        System.out.print(sigma.get(sigma.size()-1)+"}");
+    }
 }
