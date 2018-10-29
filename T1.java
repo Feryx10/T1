ca79dffadaa2855883800377ea0e913df3c1eb2c
lucianoags <lnicolas15@alumnos.utalca.cl>
2018-10-29 00:46:28 -0300

    Update T1.java

--- T1.java
+++ T1.java
@@ -13,28 +13,19 @@ public class T1 {
 
     public static void main(String[] args) throws FileNotFoundException { 
                
-       File file = new File(args[0]);
-       Scanner reader  = new Scanner (file);
-       Converter temp  = new Converter();
-       
-       String  regular = reader.nextLine();
-       String  chain   = reader.nextLine();
-       
-       String[] aux1   = regular.split("");
-       String[] aux2   = chain.split("");
-       
-       int i = 0,j = 0;
-       System.out.println("");
-       while (aux1.length!=i)
-            System.out.print(" " + aux1[i++] + " ");
-       System.out.println("");
-       while (aux2.length!=j)
-            System.out.print(" " + aux2[j++] + " ");
-       System.out.println("");
-       System.out.println("");
-       temp.toAFND(aux1,aux2);  
-       System.out.println("");
-       temp.toAFD(aux1,aux2); 
+        File file = new File(args[0]);
+        Scanner reader  = new Scanner (file);
+        Converter temp  = new Converter();
+
+        String  regular = reader.nextLine();
+        String  chain   = reader.nextLine();
+
+        String[] aux1   = regular.split("");
+        String[] aux2   = chain.split("");
+        int i = 0,j = 0;
+
+        temp.toAFND(aux1,aux2); 
+        temp.toAFD(aux1,aux2); 
     }
     
 }
